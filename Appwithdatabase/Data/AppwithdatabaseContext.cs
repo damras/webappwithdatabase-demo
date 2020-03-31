389a6bdda2507adc2f895f53893fe806bd142373
Sarmad Raza <s.raza@ict.com.qa>
2020-03-31 13:38:56 -0700

    Intial commit

---
+++ Appwithdatabase/Data/AppwithdatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Appwithdatabase.Models;
+
+namespace Appwithdatabase.Data
+{
+    public class AppwithdatabaseContext : DbContext
+    {
+        public AppwithdatabaseContext (DbContextOptions<AppwithdatabaseContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+
+        }
+
+        public DbSet<Appwithdatabase.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ Appwithdatabase/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Appwithdatabase.Models
+{
+    public class Employee
+    {
+
+       public int Id { get; set; }
+
+        public String Fullname { get; set; }
+
+        public String Department { get; set; }
+
+        public String Email { get; set; }
+    }
+}
